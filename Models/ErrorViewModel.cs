70ffc789b63d2b2a3e6222b702844ba0c6b189ee
JBitar <JBitar@anaheim.net>
2024-04-17 12:46:45 -0700

    New project pushed

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GUN_RANGE_APP_V4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
